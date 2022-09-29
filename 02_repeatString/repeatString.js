ca41b744094f886e3fe7033c68135db95eb34cba
one987 <joelbed@gmail.com>
2022-09-29 12:44:58 -0400

    first two exercises completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    
+    let repeatedString = "";
 
-};
+    while (num > 0) {
+        repeatedString += str;
+        num--;
+    }
+    return repeatedString;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+
+
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', getRandomInt(100))
+repeatString('', 10);
+
 
 // Do not edit below this line
 module.exports = repeatString;
