bbdd1182439aa35138d3d538cf414bea73256c52
one987 <joelbed@gmail.com>
2022-09-30 12:55:39 -0400

    completed tests 3, 4 and 5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+function reverseString(str) {
 
-};
+    let splitString = str.split('');
+
+    let reverseArray = splitString.reverse();
+
+    let joinArray = reverseArray.join('');
+
+    return joinArray;
+}
+
+reverseString('Hello');
+reverseString('Hello there');
+reverseString('123! abc!');
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+//const sumAll = function(num1, num2) {}
+    
+function sumAll(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof (num2) !== 'number' || typeof (num1) !== 'number') {
+        return 'ERROR';
+    } else {
+        if (num1 < num2) {
+            n1 = num1;
+            n2 = num2;
+        } else {
+            n1 = num2;
+            n2 = num1;
+        }
+        return (n1 + n2) * (n2 / 2);
+    }
+}
 
-};
+  sumAll(1, 4);
+  sumAll(1, 4000);
+  sumAll(123, 1);
+  sumAll(-10, 4);
+  sumAll(10, "90");
+  sumAll(10, [90, 1]);
+
 
 // Do not edit below this line
 module.exports = sumAll;
