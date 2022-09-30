fc348fb8e11912c992b705be245782be875ebbaf
one987 <joelbed@gmail.com>
2022-09-30 14:29:19 -0400

    finished exercises for JS fundamentals part 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+function leapYears(year) {
+    
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+        return true;
+    }
+    else if (year % 4 == 0 && year % 100 !==0 && year) {
+        return true;
+    }
+    else if (year % 100 == 0 && year % 400 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
-};
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,24 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let fTemp= far;
+  let fToCel = (fTemp - 32) * 5/9;
+  let tempCel = Math.round(fToCel * 10) / 10;
+  return tempCel;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let cTemp = cel;
+  let cToFahr = cTemp * 9 / 5 + 32;
+  let tempFahr = Math.round(cToFahr * 10) / 10;
+  return tempFahr;
 };
+
+ftoc(32);
+ftoc(100);
+ftoc(-100);
+
+ctof(0);
+ctof(73.2);
+ctof(-10);
 
 // Do not edit below this line
 module.exports = {
