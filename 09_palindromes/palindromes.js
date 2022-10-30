ed0076b9bc9bcc977b132b24536ebc48940d191c
one987 <joelbed@gmail.com>
2022-10-30 00:10:09 -0400

    added comment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,5 @@
 const palindromes = function (str) {
+    //function takes string removes punctuation, spaces and converts top lowercase then checks if it matches with itself backwards
     if (str.toLowerCase().replace(/\s+/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") == 
     str.split("").reverse().join("").toLowerCase().replace(/\s+/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")) {
         return true;
