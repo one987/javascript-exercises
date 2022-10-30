52245f899be3f3206c38e21d69b447b455d979d0
one987 <joelbed@gmail.com>
2022-10-29 23:45:29 -0400

    solved palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    if (str.toLowerCase().replace(/\s+/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") == 
+    str.split("").reverse().join("").toLowerCase().replace(/\s+/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")) {
+        return true;
+    } else return false;
+ };
 
 // Do not edit below this line
 module.exports = palindromes;
