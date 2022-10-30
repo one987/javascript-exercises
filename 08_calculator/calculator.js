85256d2ae49f39a31d24bfba1fedb312796eea2a
one987 <joelbed@gmail.com>
2022-10-29 22:58:59 -0400

    fixed var to let

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -30,7 +30,7 @@ const power = function(x, y) {
 const factorial = function(num) {
   if (num === 0 || num === 1)
     return 1;
-  for (var i = num - 1; i >= 1; i--) {
+  for (let i = num - 1; i >= 1; i--) {
     num *= i;
   }
   return num;
