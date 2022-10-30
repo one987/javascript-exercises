02b93a83f4a361649a9e29c1dbfaf13adba73145
one987 <joelbed@gmail.com>
2022-10-29 22:57:48 -0400

    solved all the math functions, yay math

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
+const add = function(a, b) {
+return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
+};
+
+const sum = function(array) {
+  let sum=0;
+    for (let i=0; i<array.length; i++) {
+        sum = sum + array[i];
+    } 
+    return sum;
 	
 };
 
-const subtract = function() {
+const multiply = function(array) {
+  let sum=1;
+    for (let i=0; i<array.length; i++) {
+        sum = sum * array[i];
+    } 
+    return sum;
+}; //thanks stack overflow
+
+const power = function(x, y) {
+	return Math.pow(x, y);
+};
+
+const factorial = function(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+}; 
 
 // Do not edit below this line
 module.exports = {
